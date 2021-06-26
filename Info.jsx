9a162eaeb108d2e029b1cc29581dfd62d05d4e6b
RamyaaD <86522270+RamyaaD@users.noreply.github.com>
2021-06-26 18:56:46 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className={"note"}>
+      <h1> Javascript and React.js </h1>
+      <p>
+        This was a bootcamp taken up by Shaurya Sinha on basic web development.
+      </p>
+    </div>
+  );
+}
+export default Info;
